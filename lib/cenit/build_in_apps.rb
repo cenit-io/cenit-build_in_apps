b3bc744da7a224d7306ad46ed19ec6c2708f6c5b
Maikel Arcia Carrazana <macarci@gmail.com>
2021-06-05 12:48:04 -0400

    Add | Precompile assets config

--- lib/cenit/build_in_apps.rb
+++ lib/cenit/build_in_apps.rb
@@ -25,6 +25,29 @@ module Cenit
       engine_dir = File.dirname(call_stack.detect { |p| p !~ %r[railties[\w.-]*/lib/rails|rack[\w.-]*/lib/rack] })
 
       engine.called_from = engine_dir
+
+      assets_dir = engine_dir.split('/')
+      assets_dir.pop(2)
+      assets_dir = "#{assets_dir.join('/')}/app/assets"
+
+      assets = []
+
+      %w(javascripts stylesheets images).each do |dir|
+        dir = "#{assets_dir}/#{dir}"
+        Dir["#{dir}/**/*"].each do |file|
+          next unless File.file?(file)
+          if file.ends_with?('.scss')
+            file = file[0..(file.length - 6)]
+          end
+          assets << file[(dir.length + 1)..file.length]
+        end
+      end
+
+      unless assets.empty?
+        engine.initializer :assets do
+          Rails.application.config.assets.precompile += assets
+        end
+      end
     end
 
     module_function
